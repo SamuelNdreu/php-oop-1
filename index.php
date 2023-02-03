8b90d1515ea03400dcad03e771b9287abf427ecd
SamuelNdreu <samwellsoficcial@gmail.com>
2023-02-03 16:23:59 +0100

    created list with movies to print

--- index.php
+++ index.php
@@ -20,7 +20,15 @@ require_once __DIR__ . '/models/Movies.php';
         <h1>
             PHP OOP 1
         </h1>
-
+        <section>
+            <div class="moviesprint">
+                <?php
+                $IRobot = new Movies('IRobot', '2022', 'Fantasy', 'Neo Matrix');
+                $IRobotPartII = new Movies('IRobot (The Return Of Blue Red Pill)', '2022', 'Fantasy', 'Neo Matrix');
+                $IRobotPartIII = new Movies('IRobot (The End Is Near', '2022', 'Fantasy', 'Neo Matrix', 'Tate /s brothers');
+                ?>
+            </div>
+        </section>
     </main>
 </body>
 
