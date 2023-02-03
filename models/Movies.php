8b16133620a7a66eb9a3cdd10805739e5492edbe
SamuelNdreu <samwellsoficcial@gmail.com>
2023-02-03 16:10:57 +0100

    created objects inside Movies Class

---
+++ models/Movies.php
@@ -0,0 +1,16 @@
+<?php
+class Movies
+{
+    public $title;
+    public $year;
+    public $genre;
+    public $director;
+
+    function __construct($_title, $_year, $_genre, $_director)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->genre = $_genre;
+        $this->director = $_director;
+    }
+}
