948c2fcde6591137b5a57f6d7d9d4eaceabd5f52
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-19 13:30:56 +0200

    implemented fuction class

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    require_once __DIR__ . '/models/Movie.php'
 ?>
 
 
@@ -11,6 +11,12 @@
     <title>Php OOP1</title>
 </head>
 <body>
-    
+    <?php 
+        $backToTheFuture = new Movie("Back To The future", "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "science fiction");
+        $titanic = new Movie("Titanic", "Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "Romantic");
+
+        var_dump($backToTheFuture, $titanic);
+        var_dump($titanic->getName());
+    ?>
 </body>
 </html>
