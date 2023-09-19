6c54810d260eec7903f07e05b2f2bb02531ab0c6
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-19 13:28:40 +0200

    creation of another php file with movies

---
+++ movie.php
@@ -0,0 +1,18 @@
+<?php 
+    class Movie{
+        public $title;
+        public $plot;
+        public $genre;
+        function __construct( $_title, $_plot, $_genre)
+        {
+        $this->title = $_title;
+        $this->plot = $_plot;
+        $this->genre = $_genre;
+        }
+
+        public function getName(){
+        return $this->title;
+        }
+    }
+
+?>
